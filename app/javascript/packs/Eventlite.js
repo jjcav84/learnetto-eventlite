9542562f11fa24058bc52def8962cdbae765c442
jjcav84 <jjcav84@gmail.com>
2022-06-09 00:36:50 -0500

    events with events list and event component displayed with date and venue

---
+++ app/javascript/packs/Eventlite.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import EventsList from './EventsList'
+
+const Eventlite = props => (
+  <div>
+    <EventsList events={props.events} />
+  </div>
+)
+
+document.addEventListener('DOMContentLoaded', () => {
+  const node = document.getElementById('events_data')
+  const data = JSON.parse(node.getAttribute('data'))
+  ReactDOM.render(
+    <Eventlite events={data} />,
+    document.body.appendChild(document.createElement('div')),
+  )
+})
