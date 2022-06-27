28915b212e03e198d8d0313800925dc7f30d950e
jjcav84 <jjcav84@gmail.com>
2022-06-27 00:11:03 -0500

    Generalized Form Validations part 1

--- app/javascript/packs/Eventlite.js
+++ app/javascript/packs/Eventlite.js
@@ -11,9 +11,9 @@ class Eventlite extends React.Component {
     super(props)
     this.state = {
       events: this.props.events,
-      title: '',
-      start_datetime: '',
-      location: '',
+      title: {value: '', valid: false},
+      start_datetime: {value: '', valid: false},
+      location: {value: '', valid: false},
       formErrors: {},
       formValid: false
     }
@@ -23,34 +23,15 @@ class Eventlite extends React.Component {
     e.preventDefault()
     const name = e.target.name
     const newState = {}
-    newState[name] = e.target.value
+    newState[name] = { ...this.state[name], value: e.target.value}
     this.setState(newState, this.validateForm)
   }
 
-  validateForm() {
-    let formErrors = {}
-    let formValid = true
-    if (this.state.title.length <= 2) {
-      formErrors.title = ["is too short (minimum is 3 characters)"]
-      formValid = false
-    }
-    if (this.state.location.length === 0) {
-      formErrors.location = ["can't be blank"]
-      formValid = false
-    }
-    if (this.state.start_datetime.length === 0) {
-      formErrors.start_datetime = ["can't be blank"]
-      formValid = false
-    } else if (Date.parse(this.state.start_datetime) <= Date.now()) {
-      formErrors.start_datetime = ["can't be in the past"]
-      formValid = false
-    }
-    this.setState({ formValid: formValid, formErrors: formErrors })
-  }
+
 
   handleSubmit = e => {
     e.preventDefault()
-    let newEvent = { title: this.state.title, start_datetime: this.state.start_datetime, location: this.state.location }
+    let newEvent = { title: this.state.title.value, start_datetime: this.state.start_datetime.value, location: this.state.location.value }
     axios({
       method: 'POST',
       url: '/events',
@@ -67,6 +48,57 @@ class Eventlite extends React.Component {
         console.log(error.response.data)
         this.setState({ formErrors: error.response.data })
       })
+  }
+
+  validateField(fieldName, fieldValue) {
+    let fieldValid = true
+    let errors = []
+    switch(fieldName) {
+      case 'title':
+      if(fieldValue.length <= 2) {
+        errors = errors.concat(["is too short (minimum is 3 characters)"])
+        fieldValid = false
+      }
+      break;
+
+      case 'location':
+      if(fieldValue.length === 0) {
+        errors = errors.concat(["can't be blank"])
+        fieldValid = false
+      }
+      break;
+
+      case 'start_datetime':
+      if(fieldValue.length === 0) {
+        errors = errors.concat(["can't be blank"])
+        fieldValid = false
+      } else if(Date.parse(fieldValue) <= Date.now()) {
+        errors = errors.concat(["can't be in the past"])
+        fieldValid = false
+      }
+      break;
+    }
+  }
+
+  validateForm() {
+    let formErrors = {}
+    let formValid = true
+    if (this.state.title.value.length <= 2) {
+      formErrors.title = ["is too short (minimum is 3 characters)"]
+      formValid = false
+    }
+    if (this.state.location.value.length === 0) {
+      formErrors.location = ["can't be blank"]
+      formValid = false
+    }
+    if (this.state.start_datetime.value.length === 0) {
+      formErrors.start_datetime = ["can't be blank"]
+      formValid = false
+    } else if (Date.parse(this.state.start_datetime.value) <= Date.now()) {
+      formErrors.start_datetime = ["can't be in the past"]
+      formValid = false
+    }
+    this.setState({ formValid: formValid, formErrors: formErrors })
   }
 
   resetFormErrors() {
@@ -87,9 +119,9 @@ class Eventlite extends React.Component {
         <EventForm handleSubmit={this.handleSubmit}
           handleInput={this.handleInput}
           formValid={this.state.formValid}
-          title={this.state.title}
-          start_datetime={this.state.start_datetime}
-          location={this.state.location} />
+          title={this.state.title.value}
+          start_datetime={this.state.start_datetime.value}
+          location={this.state.location.value} />
         <EventsList events={this.state.events} />
       </div>
     )
